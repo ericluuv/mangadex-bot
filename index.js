ef38b7570edde30f40d3ca68a29708fca8e3eb0b
ericluuv <ericluuv@gmail.com>
2022-04-07 15:14:58 -0700

    Added connection code for heroku.

--- index.js
+++ index.js
@@ -1,59 +1,72 @@
 "use strict";
 
+require('dotenv').config();
 // discord API
 const Discord = require('discord.js');
 const { MessageAttachment, MessageEmbed } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const bot = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_PRESENCES"] });
-require('dotenv').config();
 bot.login(process.env.DISCORD_TOKEN);
 
 // request API
 const fetch = require('node-fetch');
+
 // postgreSQL API
 const { Pool, Client } = require('pg');
-
-//MagnaDex URL
-const mangadex_url = "https://api.mangadex.org";
-const listId = 'a1c6b4c7-d6cc-4a82-97a7-506825bf81c4';
-
 const pool = new Pool({
-  user: 'postgres',
-  database: 'postgres',
-  password: '0000',
-  port: 5432,
-  server: 'localhost'
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+pool.connect().then(console.log('PostgreSQL connected.'));
+
+//Session & Refresh token table
+const makeMangaTable = 'CREATE TABLE IF NOT EXISTS dex_tokens ( \
+  da_key BIGINT PRIMARY KEY,  \
+  session_token TEXT,         \
+  refresh_token TEXT,         \
+  session_date BIGINT,        \
+  refresh_date BIGINT         \
+  )';
+
+pool.query(makeMangaTable, (err, res) => {
+  if (err) { console.log(err); }
+  else { console.log('dex_tokens table made successfully'); }
 });
 
-pool.connect().then(console.log('connected to database'))
 
 
-bot.on('ready', () => {
-  console.log("Bot logged in");
 
-  
+//MagnaDex Stuff
+const mangadex_url = "https://api.mangadex.org";
+const listId = 'a1c6b4c7-d6cc-4a82-97a7-506825bf81c4';
+const { getTitleInfo, updateMangaList } = require('./manga.js');
+
+
+bot.on('ready', async () => {
+  console.log("Mangadex-bot logged in");
+
   let commands;
   const guild = bot.guilds.cache.get(process.env.GUILD_ID);
   if (guild) { commands = guild.commands; }
   else { commands = bot.application?.commands; }
-  
+
 });
 
 
 
 const addCommand = new SlashCommandBuilder()
-.setName('add')
-.setDescription('Adds manga to be tracked via URL.')
-.addStringOption((option) => {
-  return option.setName('url')
-  .setDescription('The URL of the manga').setRequired(true);
-});
+  .setName('add')
+  .setDescription('Adds manga to be tracked via URL.')
+  .addStringOption((option) => {
+    return option.setName('url')
+      .setDescription('The URL of the manga').setRequired(true);
+  });
 
 const delCommand = new SlashCommandBuilder()
-.setName('delete')
-.setDescription('Adds manga to be deleted via URL.')
-.addStringOption((option) => {
-  return option.setName('url')
-  .setDescription('The URL of the manga').setRequired(true);
-});
+  .setName('delete')
+  .setDescription('Adds manga to be deleted via URL.')
+  .addStringOption((option) => {
+    return option.setName('url')
+      .setDescription('The URL of the manga').setRequired(true);
+  });
 
