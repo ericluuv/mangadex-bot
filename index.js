9b3ef18f160b53b3ca70a1e284f6f55f726264d5
ericluuv <ericluuv@gmail.com>
2022-04-10 14:21:43 -0700

    Forgot to undo local pool connection code.

--- index.js
+++ index.js
@@ -14,10 +14,8 @@ const fetch = require('node-fetch');
 // postgreSQL API
 const { Pool, Client } = require('pg');
 const pool = new Pool({
-  /*
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }*/
-  connectionString: process.env.DATABASE_URL
+  ssl: { rejectUnauthorized: false }
 });
 pool.connect().then(console.log('PostgreSQL connected.'));
 
